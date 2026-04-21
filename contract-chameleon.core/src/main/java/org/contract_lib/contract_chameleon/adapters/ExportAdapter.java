fbde7fa389c42fdbcd2cbe72f0781c0a43a4e2bb
Robert Brune <robert.brune@student.kit.edu>
2026-04-21 19:29:11 +0200

    Robert/adapter support/checker adapter (#33)
    * Simplify export adapter, reduce redundancy

--- contract-chameleon.core/src/main/java/org/contract_lib/contract_chameleon/adapters/ExportAdapter.java
+++ contract-chameleon.core/src/main/java/org/contract_lib/contract_chameleon/adapters/ExportAdapter.java
@@ -1,108 +1,51 @@
 
 package org.contract_lib.contract_chameleon.adapters;
 
-import java.util.List;
-import java.io.Writer;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Optional;
 
-import org.contract_lib.contract_chameleon.Adapter;
-import org.contract_lib.contract_chameleon.error.ChameleonException;
-import org.contract_lib.contract_chameleon.error.ChameleonMessageManager;
+import org.contract_lib.contract_chameleon.contexts.ResultDirectoryContext;
+import org.contract_lib.contract_chameleon.contexts.SourcePathsContext;
+import org.contract_lib.contract_chameleon.contexts.ResultDirectoryContext.Dir;
 
-//TODO: Rename to `TranslationAdapter` and remove Export-/Import-Adapter
-public abstract class ExportAdapter extends Adapter {
+public abstract class ExportAdapter extends TranslationAdapter {
 
-  public abstract List<TranslationResult> perform(
-      List<Path> sourceFiles,
-      ChameleonMessageManager messageManager) throws IOException;
-
-  //public abstract String adapterTitle();
-  public abstract String defaultOutputDir();
-
-  private ChameleonMessageManager messageManager = new ChameleonMessageManager();
-
-  /** The checker adapters that should run beforehand this adapter is called.
-   * <p>
-   * The adapters might run in arbitrary order,
-   * or might be skipped.
-   * <p>
-   * Defaults to a empty list of contexts.
-   */
-  public List<CheckerAdapter> requiredAdapters() {
-    return List.of();
-  }
+  private ResultDirectoryContext result;
 
   @Override
-  public final void perform() {
-    //if (args.length <= 1) {
-    //  System.err.println("Expected path to files in command"); //TODO: proper error handling
-    //  return;
-    //}
-    String inputFileName = ""; //= args[1];
-    //if (args.length > 2) {
-    //  System.err.println("Only the first input file is handled at the moment.");
-    //}
-    System.err.println("This is an old interface, please change to 'TranslationAdapter'.");
+  public final void performTranslation() {
 
-    try {
-      List<TranslationResult> results = this.perform(List.of(Paths.get(inputFileName)), messageManager);
-      // Create adapter directory
-      String outputDir = defaultOutputDir();
-      String fileDir = getDirForFile(inputFileName);
-      Path directoryPath = Paths.get(".", outputDir, fileDir);
-      if (Files.isDirectory(directoryPath)) {
-        System.err.println(String.format("INFO: Directory at %s does already exist.", directoryPath));
-      } else {
-        Files.createDirectories(directoryPath);
-      }
+    Optional<SourcePathsContext> sourcesContext = getContext(SourcePathsContext.class);
+    Optional<ResultDirectoryContext> resultContext = getContext(ResultDirectoryContext.class);
+    if (sourcesContext.isEmpty()) {
+      getMessageContext().logError("Interface: ExportAdapters requires SourcePathsContext.");
+      return;
+    }
+    if (resultContext.isEmpty()) {
+      getMessageContext().logError("Interface: ExportAdapters requires ResultDirectoryContext required.");
+      return;
+    }
+    this.result = resultContext.get();
 
-      //Write results
-      for (TranslationResult res : results) {
-        String directoryName = res.directoryName();
-        String fileName = res.fileName();
-        String fileEnding = res.fileEnding();
-        Path packagePath = Paths.get(".", outputDir, fileDir, directoryName);
-        Path classPath = Paths.get(".", outputDir, fileDir, directoryName, fileName + fileEnding);
-
-        if (Files.isDirectory(packagePath)) {
-          System.err.println(String.format("INFO: Directory at %s does already exist.", packagePath));
-        } else {
-          Files.createDirectories(packagePath);
-        }
-
-        if (Files.exists(classPath)) {
-          System.err.println(String.format("WARNING: File at %s does already exist, will be overridden!", classPath));
-        }
-
-        try (BufferedWriter writer = Files.newBufferedWriter(classPath)) {
-          res.write(writer);
-        }
-      }
-    } catch (IOException e) {
-      messageManager.report(new ChameleonException(e));
+    if (sourcesContext.get().getPaths().size() == 1) {
+      this.performForPath(sourcesContext.get().getPaths().getFirst(), this.result.getResultDirectory());
+    } else {
+      sourcesContext.get().getPaths().forEach(this::performAddingSubdir);
     }
   }
 
-  //TODO: find better solution
-  private String getDirForFile(String file) {
-    return file.replaceAll("(\\.|/)", "_");
+  final void performAddingSubdir(Path p) {
+    String filename = p.getFileName().toString();
+    Dir finalDir = result.getResultDirectory().addSubDirectories(filename);
+    //NOTE: As there might be multiple Contract-LIB files that have the same name, this will just override I think.
+    performForPath(p, finalDir);
   }
 
-  public interface TranslationResult {
-    /// The name of the directory where the file should be stored.
-    String directoryName();
+  /** Given a path to a single ContractLIB file, creates a translation in the result directory.
+   *
+   * @param p the path to the ContractLIB file.
+   * @param finalDir the directory where the results should be written.
+   */
+  public abstract void performForPath(Path p, Dir finalDir);
 
-    /// The name under which the file should be stored. 
-    String fileName();
-
-    /// The fileending of the file stored.
-    String fileEnding();
-
-    /// Write handler how the content should be written to the file.
-    void write(Writer writer) throws IOException;
-  }
 }
